cdb105be87a8e7b4bf5eb1637b7e3787b1d62308
Daniel Šrom <124529843+DanielSrom@users.noreply.github.com>
2023-09-18 17:26:04 +0200

    Add files via upload

--- FaceDetect.py
+++ FaceDetect.py
@@ -1,30 +1,42 @@
 import cv2 as cv
+import argparse as argp
 
 #Import pretrained model
 faceCascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-#Import image + convert to gray
-src = cv.imread('detect.jpg')
-gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-
-faces = faceCascade.detectMultiScale(gray, scaleFactor=1.4, minNeighbors=4)
-
-for (x, y, w, h) in faces:
-    border_size = 20
-    x_border = max(0, x - border_size)
-    y_border = max(0, y - border_size)
-    w_border = min(src.shape[1] - x, w + 2 * border_size)
-    h_border = min(src.shape[0] - y, h + 2 * border_size)
-    cv.rectangle(src, (x_border, y_border), (x_border + w_border, y_border + h_border), (255, 211, 42), 10)
-
-#Smaller output
-percent = 20
-width = int(src.shape[1] * percent / 100)
-height = int(src.shape[0] * percent / 100)
-dim = (width, height)
-new_src = cv.resize(src, dim)
 
 
-cv.imshow('Output', new_src)
-cv.waitKey(0)
-cv.destroyAllWindows()
+def detectFaces(picture):
+    #Import pretrained model
+    faceCascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+    #Import image + convert to gray
+    src = cv.imread(picture)
+    gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.4, minNeighbors=4)
+
+    for (x, y, w, h) in faces:
+        border_size = 20
+        x_border = max(0, x - border_size)
+        y_border = max(0, y - border_size)
+        w_border = min(src.shape[1] - x, w + 2 * border_size)
+        h_border = min(src.shape[0] - y, h + 2 * border_size)
+        cv.rectangle(src, (x_border, y_border), (x_border + w_border, y_border + h_border), (255, 211, 42), 10)
+
+    #Smaller output
+    percent = 20
+    width = int(src.shape[1] * percent / 100)
+    height = int(src.shape[0] * percent / 100)
+    dim = (width, height)
+    new_src = cv.resize(src, dim)
+
+
+    cv.imshow('Output', new_src)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+if __name__ == '__main__':
+  parser = argp.ArgumentParser(description='App to detect Faces')
+  parser.add_argument("-p", "--picture", action="store", dest="picture", default="detect.jpg")
+  arguments = parser.parse_args()
+  detectFaces(arguments.picture)
